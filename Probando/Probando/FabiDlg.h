1bc0bfad97dadf69a240b684e01dc969ace92b10
SaFabi <ti13110050@itess.edu.mx>
2017-04-08 13:16:56 -0500

    commitFabi

--- Probando/Probando/FabiDlg.h
+++ Probando/Probando/FabiDlg.h
@@ -1,7 +1,7 @@
 #pragma once   //_____________________________________________ FabiDlg.h  
 #include "resource.h"
 
-class FabiDlg: public Win::Dialog
+class FabiDlg : public Win::Dialog
 {
 public:
 	FabiDlg()
@@ -12,10 +12,30 @@ public:
 	}
 private:
 	//______ Wintempla GUI manager section begin: DO NOT EDIT AFTER THIS LINE
-	void Window_Open(Win::Event& e);
+	Win::Textbox tbxMsj;
+	Win::Label lb1;
+protected:
+	Win::Gdi::Font fontArial009A;
 	void GetDialogTemplate(DLGTEMPLATE& dlgTemplate)
 	{
-		dlgTemplate.style = DS_CENTER | DS_MODALFRAME | WS_POPUP | WS_VISIBLE | WS_CAPTION | WS_SYSMENU;
-
+		dlgTemplate.cx = Sys::Convert::CentimetersToDlgUnitX(10.16000);
+		dlgTemplate.cy = Sys::Convert::CentimetersToDlgUnitY(2.54000);
+		dlgTemplate.style = WS_CAPTION | WS_POPUP | WS_SYSMENU | WS_VISIBLE | DS_CENTER | DS_MODALFRAME;
+	}
+	//_________________________________________________
+	void InitializeGui()
+	{
+		tbxMsj.CreateX(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | ES_AUTOHSCROLL | ES_LEFT | ES_WINNORMALCASE, 4.04812, 1.74625, 5.92667, 0.60854, hWnd, 1000);
+		lb1.CreateX(NULL, L"Mensaje", WS_CHILD | WS_VISIBLE | SS_LEFT | SS_WINNORMAL, 1.42875, 1.66688, 2.19604, 0.60854, hWnd, 1001);
+		fontArial009A.CreateX(L"Arial", 0.317500, false, false, false, false);
+		tbxMsj.Font = fontArial009A;
+		lb1.Font = fontArial009A;
+	}
+	//_________________________________________________
+	void Window_Open(Win::Event& e);
+	//_________________________________________________
+	bool EventHandler(Win::Event& e)
+	{
+		return false;
 	}
 };
--- Probando/Probando/backup.h
+++ Probando/Probando/backup.h
@@ -1,37 +1,41 @@
-#pragma once  //______________________________________ Probando.h  
-#include "Resource.h"
-#include "FabiDlg.h"
-#include "AdiiDlg.h"
-class Probando: public Win::Window
+#pragma once   //_____________________________________________ FabiDlg.h  
+#include "resource.h"
+
+class FabiDlg: public Win::Dialog
 {
 public:
-	Probando()
+	FabiDlg()
 	{
 	}
-	~Probando()
+	~FabiDlg()
 	{
 	}
-	const wchar_t * GetClassName(){return L"Probando";}
-	Win::Menu menu;
-protected:
+private:
 	//______ Wintempla GUI manager section begin: DO NOT EDIT AFTER THIS LINE
+	Win::Textbox tbxMsj;
+	Win::Label lb1;
 protected:
-	void GetWindowInformation(CREATESTRUCT& createStruct)
+	Win::Gdi::Font fontArial009A;
+	void GetDialogTemplate(DLGTEMPLATE& dlgTemplate)
 	{
-		createStruct.style = WS_CLIPCHILDREN | WS_OVERLAPPEDWINDOW;
+		dlgTemplate.cx=Sys::Convert::CentimetersToDlgUnitX(10.16000);
+		dlgTemplate.cy=Sys::Convert::CentimetersToDlgUnitY(2.56646);
+		dlgTemplate.style = WS_CAPTION | WS_POPUP | WS_SYSMENU | WS_VISIBLE | DS_CENTER | DS_MODALFRAME;
 	}
 	//_________________________________________________
 	void InitializeGui()
 	{
-		this->Text = L"Probando";
+		tbxMsj.CreateX(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | ES_AUTOHSCROLL | ES_LEFT | ES_WINNORMALCASE, 4.04812, 1.74625, 5.92667, 0.60854, hWnd, 1000);
+		lb1.CreateX(NULL, L"Mensaje", WS_CHILD | WS_VISIBLE | SS_LEFT | SS_WINNORMAL, 1.27000, 1.77271, 2.19604, 0.60854, hWnd, 1001);
+		fontArial009A.CreateX(L"Arial", 0.317500, false, false, false, false);
+		tbxMsj.Font = fontArial009A;
+		lb1.Font = fontArial009A;
 	}
 	//_________________________________________________
 	void Window_Open(Win::Event& e);
-	void Cmd_About(Win::Event& e);
 	//_________________________________________________
 	bool EventHandler(Win::Event& e)
 	{
-		if (this->IsEvent(e, IDM_ABOUT)) {Cmd_About(e); return true;}
 		return false;
 	}
 };
